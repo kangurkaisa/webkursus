24e9c9f41df5b1b3acecbec78d7ad9d74b3edadf
Kaisa Kangur <kangurkaisa@gmail.com>
2022-02-09 18:04:44 +0200

    add buttons Bulma

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
-<button v-on:click="clicks++">click me</button> {{ clicks }}<br>
+<div class="container">
+    <section class="section">
+        <span class="tagi-is-primary">Primary</span>
+<button class="button is-primary" v-on:click="clicks++">click me</button>
+ {{ clicks }}
+ <br>
 <!-- v-model seotud message väärtusega ja see väärtus on rektiivne -->
 <input type="text" v-model="message">  
-  <h1>{{ message }}</h1>
+  <h1 class="is-size-1">{{ message }}</h1>
 <br>
-<h1>Shopping List</h1>
-<input type="text" v-model="newItem" @keypress.enter="addItem">
-<button @click="addItem()" :disabled="this.newItem.trim() === ''">Add</button>
+<h1 class="is-size-1">Shopping List</h1>
+<div class="field has-addons">
+  <div class="control">
+    <input class="input" type="text" placeholder="New item" v-model="newItem" @keypress.enter="addItem">
+  </div>
+  <div class="control">
+    <button class="button is-info" @click="addItem()" :disabled="this.newItem.trim() === ''">
+        Add
+    </button>
+  </div>
+</div>
 <ul>
-    <li v-for="(item,index) in items" :key="index">
-       <button>delete</button>
+    <li class="mt-2" v-for="(item,index) in items" :key="index">
+        <button  class="button is-primary is-small" @click="items.splice(index, 1)">delete</button>
         {{item}}
+       
    </li>
 </ul>
+    </section>
+</div>
 </template>
 
 <script>
